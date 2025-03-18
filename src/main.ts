57365733ccc7679beb17be28d5b688dc9dc962bd
hzlshen <m15277968105@163.com>
2025-03-18 20:25:50 +0800

    feat : add 增加vue3子应用

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,14 @@ import App from "./App.vue";
 import { registerMicroApps,start } from "qiankun";
 
 createApp(App).mount("#app");
+
+registerMicroApps([
+  {
+    name:'sub-vue', //  名称
+    entry: '//localhost:8888', // 子应用入口
+    container: '#subvue-viewport', // 子应用挂载的节点
+    activeRule: '/app1', // 激活规则，当路由匹配时加载子应用
+  }
+])
+
+start()
