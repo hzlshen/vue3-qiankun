ac7b6eee34cf92c157e0a3f2230c50414967cc43
hzlshen <m15277968105@163.com>
2025-03-19 19:44:41 +0800

    add 主应用配置路由

---
+++ src/components/SubContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="subvue-viewport"></div>
+</template>
+
+<script>
+import { start } from 'qiankun'
+import { registerApps } from '@/utils/qiankun'
+export default {
+  mounted() {
+    if (!window.qiankunStarted) {
+      window.qiankunStarted = true
+      registerApps()
+      start({
+        sandbox: {
+          experimentalStyleIsolation: true // 样式隔离
+        }
+      })
+    }
+  }
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,29 @@
-// src/router/index.js 或 src/router/index.ts
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../App.vue';
-// import About from '../views/About.vue';
- 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: About,
-  // },
-];
- 
-const router = createRouter({
-  history: createWebHistory(), // 使用HTML5 History模式
-  routes, // `routes: routes` 的缩写
-});
- 
-export default router;
+    path: '',
+    redirect: { name: 'home' },
+    meta: { title: '首页' },
+    children: [
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import('../views/home/index.vue')
+      },
+      {
+        // history模式需要通配所有路由，详见vue-router文档
+        path: '/sub-vue/:pathMatch(.*)*',
+        name: 'sub-vue',
+        meta: {},
+        component: () => import('@/components/SubContainer.vue')
+      },
+      // {
+      //   path: '/app/app-vue2/',
+      //   name: 'app-vue2',
+      //   meta: {},
+      //   component: () => import('@/components/SubContainer.vue')
+      // }
+    ]
+  }
+]
+
+export default routes
